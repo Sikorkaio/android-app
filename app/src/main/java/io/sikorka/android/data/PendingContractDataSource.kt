f5ec3eaac3532c3871ac0471e6558c9ebae729ec
Konstantinos Paparas <kelsos86@gmail.com>
2017-10-14 14:36:46 +0300

    Change PendingContractDataSources to return flowable.

--- app/src/main/java/io/sikorka/android/data/PendingContractDataSource.kt
+++ app/src/main/java/io/sikorka/android/data/PendingContractDataSource.kt
@@ -4,13 +4,14 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Delete
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.Query
+import io.reactivex.Flowable
 
 
 @Dao
 interface PendingContractDataSource {
 
   @Query("SELECT * FROM pending_contracts")
-  fun getAllPendingContracts(): List<PendingContract>
+  fun getAllPendingContracts(): Flowable<PendingContract>
 
   @Insert
   fun insert(pendingContract: PendingContract)
