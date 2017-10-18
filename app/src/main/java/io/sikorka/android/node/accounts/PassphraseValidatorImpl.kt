c551d39aa010eb9b49a3d21839c6c7fe231d893e
Konstantinos Paparas <kelsos86@gmail.com>
2017-10-18 22:46:49 +0300

    Password minimum length is now ignored on debug.

--- app/src/main/java/io/sikorka/android/node/accounts/PassphraseValidatorImpl.kt
+++ app/src/main/java/io/sikorka/android/node/accounts/PassphraseValidatorImpl.kt
@@ -1,5 +1,6 @@
 package io.sikorka.android.node.accounts
 
+import io.sikorka.android.BuildConfig
 import io.sikorka.android.helpers.fail
 import javax.inject.Inject
 
@@ -7,7 +8,7 @@ class PassphraseValidatorImpl
 @Inject constructor() : PassphraseValidator {
   override fun validate(passphrase: String, passphraseConfirmation: String): Long = when {
     passphrase.isBlank() -> ValidationResult.EMPTY_PASSPHRASE
-    passphrase.length < PASSWORD_MIN_LENGTH -> ValidationResult.PASSWORD_SHORT
+    passphrase.length < PASSWORD_MIN_LENGTH && !BuildConfig.DEBUG -> ValidationResult.PASSWORD_SHORT
     passphrase != passphraseConfirmation -> ValidationResult.CONFIRMATION_MISSMATCH
     passphrase == passphraseConfirmation -> ValidationResult.OK
     else -> fail("not supported branch")
